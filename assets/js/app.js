714c4e56356c307b3feedb791075c6845e37e2f8
saefulmujibaswadasd-boop <saefulmujibaswadasd@gmail.com>
2026-02-21 17:51:32 +0700

    app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-console.log("System Ready");
+document.addEventListener("DOMContentLoaded", function(){
+
+let sqi = 82.4;
+let ikp = 84.1;
+let nilai = 78.6;
+
+document.getElementById("sqi").innerText = sqi;
+document.getElementById("ikp").innerText = ikp;
+document.getElementById("nilai").innerText = nilai;
+
+});
