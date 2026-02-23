74f17ef457931d97ddcf1abfb869bc2a5b4d6885
saefulmujibaswadasd-boop <saefulmujibaswadasd@gmail.com>
2026-02-23 19:58:29 +0700

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,27 @@
+const CONFIG = {
+  PROFIL: {
+    FILE_NAME: "SIS_2026_MASTER_PROFIL",
+    SHEET: "profil_sekolah",
+    URL: "CSV_URL_PROFIL"
+  },
+
+  TEACHERS: {
+    FILE_NAME: "SIS_2026_MASTER_TEACHERS",
+    SHEET: "teachers_master",
+    URL: "CSV_URL_TEACHERS"
+  },
+
+  STUDENTS: {
+    FILE_NAME: "SIS_2026_MASTER_STUDENTS_PUBLIC",
+    SHEET: "students_master_public",
+    URL: "CSV_URL_STUDENTS"
+  },
+
+  SUPERVISI: {
+    FILE_NAME: "SIS_2026_MASTER_SUPERVISI",
+    OBSERVASI: "supervisi_observasi",
+    INDIKATOR: "supervisi_indikator_master",
+    DETAIL: "supervisi_detail_skor",
+    URL_OBSERVASI: "PRIVATE_OR_SUMMARY_URL"
+  }
+};
