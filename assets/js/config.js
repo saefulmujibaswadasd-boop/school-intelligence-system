7136ab0fa9ccb73e0488c6272d44ccda0d04b45c
saefulmujibaswadasd-boop <saefulmujibaswadasd@gmail.com>
2026-02-24 21:42:52 +0700

    config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -19,11 +19,13 @@ const CONFIG = {
   },
 
   SUPERVISI: {
-    FILE_NAME: "SIS_2026_MASTER_SUPERVISI",
-    OBSERVASI: "supervisi_observasi",
-    INDIKATOR: "supervisi_indikator_master",
-    DETAIL: "supervisi_detail_skor",
-    URL_OBSERVASI: "URL_PRIVATE_OR_SUMMARY"
-  }
+  FILE_NAME: "SIS_2026_MASTER_SUPERVISI",
 
-};
+  OBSERVASI: "supervisi_observasi",
+  INDIKATOR: "supervisi_indikator_master",
+  DETAIL: "supervisi_detail_skor",
+
+  URL_INDIKATOR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRtkzuNu-L5Raf6IpOAQoq3DNYdZCPW33ej874QW1xpSj_XQT1bUd7RvXd_2hk8YRDBIm5boW1soHAO/pub?gid=1138840480&single=true&output=csv",
+
+  URL_OBSERVASI: "URL_PRIVATE_OR_SUMMARY"
+}
